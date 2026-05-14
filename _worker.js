00e284368866687865d9fc95178a22805806b8aa
potatosips <118026260+potatosips@users.noreply.github.com>
2026-05-14 08:42:01 +0600

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,27 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const host = url.hostname.toLowerCase();
+
+    const routes = {
+      "batt-health-mi11x.potatosips.oporajita.win": "/sites/battery-health-check-Mi11x/",
+      "remote-wol-guide.potatosips.oporajita.win": "/sites/remote-wol-guide/",
+      "another-site.potatosips.oporajita.win": "/sites/another-site/",
+    };
+
+    const targetPath = routes[host];
+
+    if (targetPath) {
+      const newUrl = new URL(request.url);
+      newUrl.pathname = targetPath;
+      return env.ASSETS.fetch(newUrl);
+    }
+
+    return new Response("Site not found", {
+      status: 404,
+      headers: {
+        "content-type": "text/plain",
+      },
+    });
+  },
+};
